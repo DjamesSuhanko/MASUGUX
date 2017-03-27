6053f460aaf91f4c537ba3125ef7f17250b2c427
DjamesSuhanko <djames.suhanko@gmail.com>
2017-03-27 18:39:41 -0300

    Novas implementações

--- goToBed.py
+++ goToBed.py
@@ -8,87 +8,119 @@ from machine import Pin
 from time import sleep_ms
 from umqtt.simple import MQTTClient
 
-class sleeper:
-    def __init__(self,bb_mqtt_id="None",mqtt_broker="None",mqtt_port=0,mqtt_user="None",mqtt_passwd="None",step="None"):
-        self.MINUTES_TO_SLEEP = 1
-        self.SLEEP_TIME       = self.MINUTES_TO_SLEEP * 10 * 1000
-        self.DONT_SLEEP       = 0
-        self.MATURATION       = 1
-        self.BREWING          = 0
-        self.STEP             = step
-        self.mqtt_broker      = mqtt_broker
-        self.mqtt_port        = mqtt_port
-        self.mqtt_user        = mqtt_user
-        self.mqtt_passwd      = mqtt_passwd
-        self.mqtt_id          = bb_mqtt_id
-        self.sta_if           = network.WLAN(network.STA_IF)
-        
-        #fermentacao
-        self.brew_low         = 17
-        self.brew_ok          = 20
-        self.brew_bit_high    = 22
-        self.brew_very_high   = 23
 
-        #maturacao
-        self.mat_low          = 4
-        self.mat_ok           = 6
-        self.mat_bit_high     = 8
-        self.mat_very_high    = 9
-        
-        #temps: low, ok, bit high, high
-        self.tempFor          = {self.BREWING:[17,20,22,23], self.MATURATION:[0,4,7,9]}
-        
-    """Rotina a ser chamada no boot.py.
-    0 - coleta temperatura e publica
-    1 - fica em loop até que seja publicado que pode dormir
-    2 - faz a rotina de dormir. Dorme no intervalo definido em self.SLEEP_TIME
-    3 - quando der o timeout, acorda, coleta, envia e repete.
-    """
-    
+class sleeper:
+    def __init__(self, bb_mqtt_id="None", mqtt_broker="None", mqtt_port=0, mqtt_user="None", mqtt_passwd="None",
+                 step="None"):
+        self.MINUTES_TO_SLEEP = 1
+        self.SLEEP_TIME = self.MINUTES_TO_SLEEP * 10 * 1000
+        self.DO_SLEEP = False
+        self.MATURATION = 1
+        self.BREWING = 0
+        self.STEP = step
+        self.mqtt_broker = mqtt_broker
+        self.mqtt_port = mqtt_port
+        self.mqtt_user = mqtt_user
+        self.mqtt_passwd = mqtt_passwd
+        self.mqtt_id = bb_mqtt_id
+        self.sta_if = network.WLAN(network.STA_IF)
+
+        # fermentacao
+        self.brew_low = 17
+        self.brew_ok = 20
+        self.brew_bit_high = 22
+        self.brew_very_high = 23
+
+        # maturacao
+        self.mat_low = 4
+        self.mat_ok = 6
+        self.mat_bit_high = 8
+        self.mat_very_high = 9
+
+        # temps: low, ok, bit high, high
+        self.tempFor = {self.BREWING: [17, 20, 22, 23], self.MATURATION: [0, 4, 7, 9]}
+
+        self.myMQTT = MQTTClient(self.mqtt_id, self.mqtt_broker, self.mqtt_port, self.mqtt_user, self.mqtt_passwd)
+
     def now(self):
+        """Rotina a ser chamada no boot.py.
+        0 - coleta temperatura e publica
+        1 - fica em loop ate que seja publicado que pode dormir
+        2 - faz a rotina de dormir. Dorme no intervalo definido em self.SLEEP_TIME
+        3 - quando der o timeout, acorda, coleta, envia e repete.
+        """
         size = 0
+        print("Estabelecendo conexao de rede...")
         while size < 11:
-          try:
-            size = len(self.sta_if.ifconfig()[0])
-            time.sleep_ms(80)
-          except:
-            size = 0
-            
-        #coleta e publica temperatura...
+            try:
+                size = len(self.sta_if.ifconfig()[0])
+                time.sleep_ms(80)
+            except:
+                size = 0
+
+        print("setting callback...")
+        self.myMQTT.set_callback(self.sub_cb)
+        print("connecting for...")
+        self.myMQTT.connect()
+
+        print("Subscribe for sleep...")
+        self.myMQTT.subscribe(b'freezer/sleep')
+        print("Publishing termometer status")
+        self.myMQTT.publish(b'freezer/termometro',b'ON')
+        # coleta e publica temperatura...
         print("Coletando temperatura e publicando...")
         t = self.tasks()
 
-        #...aguarda por ordem de dormir...
+        # ...aguarda por ordem de dormir...
         print("Aguardando ordem para dormir...")
-        self.checkIfIcanSleep()
-        print("Dormindo. Ate logo.")
+        self.waiting()
+        if self.DO_SLEEP:
+            self.myMQTT.publish("freezer/termometro", "OFF")
+            print("Dormindo. Ate logo.")
+            # ...inicia processo de deepsleep.
+            self.DO_SLEEP = False
+            rtc = machine.RTC()
+            rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
 
-        #...inicia processo de deepsleep.
-        rtc = machine.RTC()
-        rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+            # wake up after...
+            rtc.alarm(rtc.ALARM0, self.SLEEP_TIME)
 
-        #wake up
-        rtc.alarm(rtc.ALARM0, self.SLEEP_TIME)
+            # sleep
+            machine.deepsleep()
 
-        #sleep
-        machine.deepsleep()
+    def waiting(self):
+        acum = time.ticks_ms()
+        delta = time.ticks_diff(time.ticks_ms(),acum)
+        while delta < 5000:
+            delta = time.ticks_diff(time.ticks_ms(), acum)
+            time.sleep_ms(5)
 
-    #chama metodo de verificacao de temperatura e publica
+        self.DO_SLEEP = True
+        print("Ordem para dormir")
+
+    def sub_cb(self, topic, msg):
+        self.myMQTT.publish("freezer/callback", "OK")
+        if topic == b'freezer/sleep':
+            print("trying sleep")
+            if msg == b'1':
+                self.DO_SLEEP = True
+
+    # chama metodo de verificacao de temperatura e publica
     def tasks(self):
         print("Lendo temperatura...")
         temperature = self.status()
         print(temperature)
-        
+
         if not temperature is None:
             self.publish(temperature)
             return
-            
+
         print("Nao pude fazer a leitura da temperatura")
 
-    #pega a temperatura do DS18B20 e chama o metodo publish() para publicar no broker
+    # pega a temperatura do DS18B20 e chama o metodo publish() para publicar no broker
     def status(self):
-        ow   = onewire.OneWire(Pin(2))
-        ds   = ds18x20.DS18X20(ow)
+        ow = onewire.OneWire(Pin(2))
+        ds = ds18x20.DS18X20(ow)
         roms = ds.scan()
 
         if not len(roms):
@@ -102,24 +134,17 @@ class sleeper:
         for i in range(10):
             for rom in roms:
                 temp += ds.read_temp(rom)
-        return temp/10
+        return temp / 10
 
-    #envia a temperatura para o broker
-    def publish(self,valueTo):
-        myMQTT = MQTTClient(self.mqtt_id,self.mqtt_broker,self.mqtt_port,self.mqtt_user,self.mqtt_passwd)
+    # envia a temperatura para o broker
+    def publish(self, valueTo):
         try:
-            myMQTT.connect()
-            myMQTT.publish(b"beer/temperature",str(valueTo))
+            self.myMQTT.connect()
+            self.myMQTT.publish(b"beer/temperature", str(valueTo))
         except:
-            print("Nao foi possivel conectar ao broker")    
+            print("Nao foi possivel conectar ao broker")
 
-    #aguarda ate que a resposta no topico seja "1" para a variavel self.DONT_SLEEP
-    def checkIfIcanSleep(self):
-        #fazer o subscribe
-        sleep_ms(60000)
-        return
-
-    def ledControl(self,temperature):
+    def ledControl(self, temperature):
         if self.STEP == self.BREWING:
-            #blabla....
+            # blabla....
             return
--- tempMonitor.py
+++ tempMonitor.py
@@ -3,98 +3,145 @@ import time
 from umqtt.simple import MQTTClient
 from machine import Pin
 import network
+
+
 # Publish test messages e.g. with:
 # mosquitto_pub -t foo_topic -m hello
 
 class freezer:
-    def __init__(self,bb_mqtt_id="None",mqtt_broker="None",mqtt_port=0,mqtt_user="None",mqtt_passwd="None",topic="None"):
-        self.relay_one = Pin(12,Pin.OUT) 
-        self.relay_two = Pin(13,Pin.OUT)
+    def __init__(self, bb_mqtt_id="None", mqtt_broker="None", mqtt_port=0, mqtt_user="None", mqtt_passwd="None",
+                 topic="None"):
+        self.relay_one = Pin(12, Pin.OUT)
+        self.relay_two = Pin(13, Pin.OUT)
 
-        self.bb_mqtt_id   = "ElectroDragon"
-        self.mqtt_broker  = mqtt_broker
-        self.mqtt_port    = mqtt_port
-        self.mqtt_user    = mqtt_user
-        self.mqtt_passwd  = mqtt_passwd
-        self.topic        = topic
-        self.mqtt         = MQTTClient(self.bb_mqtt_id,self.mqtt_broker,self.mqtt_port,self.mqtt_user,self.mqtt_passwd)
-        self.sta_if       = network.WLAN(network.STA_IF)
+        self.bb_mqtt_id = "ElectroDragon"
+        self.mqtt_broker = mqtt_broker
+        self.mqtt_port = mqtt_port
+        self.mqtt_user = mqtt_user
+        self.mqtt_passwd = mqtt_passwd
+        self.topic = topic
+        self.mqtt = MQTTClient(self.bb_mqtt_id, self.mqtt_broker, self.mqtt_port, self.mqtt_user, self.mqtt_passwd)
+        self.sta_if = network.WLAN(network.STA_IF)
 
-        self.programs     = {b'fermentation':[18.0,22.0],b'maturation':[0.0,2.0],b'priming':[23.0,25.0]}
-        self.MINIMUM      = self.programs[b'fermentation'][0]
-        self.MAXIMUM      = self.programs[b'fermentation'][1]
-        
+        self.programs = {b'fermentation': [18.0, 22.0], b'maturation': [0.0, 2.0], b'priming': [23.0, 25.0]}
+        self.MINIMUM = self.programs[b'fermentation'][0]
+        self.MAXIMUM = self.programs[b'fermentation'][1]
+
+        self.relay_status = "OFF"
+
         print("Waiting IP...")
         size = 0
         while size < 11:
-          try:
-            size = len(self.sta_if.ifconfig()[0])
-            time.sleep_ms(80)
-          except:
-            size = 0
+            try:
+                size = len(self.sta_if.ifconfig()[0])
+                time.sleep_ms(80)
+            except:
+                size = 0
 
         self.mqtt.connect()
 
+    def ts2name(self):
+        val = str(time.localtime())
+        return val
+
+    def log(self, msg):
+        f = open("tempMonitor.log", 'a')
+        message = self.ts2name()
+        messages = message + msg
+        f.write(messages)
+        f.close()
+
     def doSleep(self):
         try:
-            #self.mqtt.connect()
-            self.mqtt.publish(b"beer/sleep",b'1')
+            #diz ao esp monitor para dormir, apos ter recebido a mensagem
+            # self.mqtt.connect()
+            self.mqtt.publish(b"freezer/sleep", b'1')
         except:
             print("Nao foi possivel conectar ao broker")
+            self.log("Nao foi possivel fazer publish\r\n ")
+            return
 
-    def toNumber(self,target):
-        if len(target) == 4:
-            temp  = (target[0] - 48) * 10.0
-            temp += (target[1] - 48) *  1.0
-            temp += (target[3] - 48) *  0.1
+    def toNumber(self, target):
+        try:
+            if len(target) == 4:
+                temp = (target[0] - 48) * 10.0
+                temp += (target[1] - 48) * 1.0
+                temp += (target[3] - 48) * 0.1
 
-        elif len(target) == 3:
-            if not target.find(b'.') == 1:
-                return 0.0
+            elif len(target) == 3:
+                if not target.find(b'.') == 1:
+                    return 0.0
 
-            temp  = (target[0] - 48) * 1.0
-            temp += (target[2] - 48) * 0.1
+                temp = (target[0] - 48) * 1.0
+                temp += (target[2] - 48) * 0.1
+
+        except:
+            self.log("Falha no calculo da temperatura")
+            return 0.0
 
         self.doSleep()
         return temp
-                
+
     # Received messages from subscriptions will be delivered to this callback
-    def sub_cb(self,topic, msg):
+    def sub_cb(self, topic, msg):
         print((topic, msg))
+        print("temperaturas do programa atual:")
+        print(self.MINIMUM)
+        print(self.MAXIMUM)
 
-        if topic == b'beer/program':
-            if msg in self.programs.keys():
-                self.MINIMAL = self.programs[msg][0]
-                self.MAXIMUM = self.programs[msg][1]
-                print("Programa escolhido:")
-                print(msg)
-                
-        elif topic == b'beer/temperature':
-            temp = self.toNumber(msg)
-            if temp > self.MAXIMUM:
-                self.relay_one.high()
-                print("relay ON")
-            
-            elif temp < self.MINIMUM:
-                print("relay OFF")
-                self.relay_one.low()
+        try:
+            if topic == b'beer/program':
+                if msg in self.programs.keys():
+                    self.MINIMAL = self.programs[msg][0]
+                    self.MAXIMUM = self.programs[msg][1]
+                    print("Programa escolhido:")
+                    print(msg)
 
-    def check(self,server="192.168.1.2"):
+            elif topic == b'beer/temperature':
+                temp = self.toNumber(msg)
+                if temp > self.MAXIMUM:
+                    self.relay_one.high()
+                    print("relay ON")
+                    self.relay_status = "ON"
+
+                elif temp < self.MINIMUM:
+                    self.relay_one.low()
+                    print("relay OFF")
+                    self.relay_status = "OFF"
+
+            self.mqtt.publish(b"freezer/relay", self.relay_status)
+
+        except:
+            self.log("Falha no callback\r\n ")
+            return
+
+
+    def check(self, server="192.168.1.2"):
         print("Connecting...")
         print("Setting callback...")
-        self.mqtt.set_callback(self.sub_cb)
-        #self.mqtt.connect()
+        try:
+            self.mqtt.set_callback(self.sub_cb)
+        except:
+            self.log("Falha em set_callback")
+            return
+
+        # self.mqtt.connect()
         print("Subscribe to beer/temperature...")
         self.mqtt.subscribe(b"beer/#")
         while True:
-            if True:
-                # Blocking wait for message
-                self.mqtt.wait_msg()
-            else:
-                # Non-blocking wait for message
-                self.mqtt.check_msg()
-                # Then need to sleep to avoid 100% CPU usage (in a real
-                # app other useful actions would be performed instead)
-                time.sleep(1)
+            try:
+                #if True:
+                #    # Blocking wait for message
+                #    self.mqtt.wait_msg()
+                #else:
+                if True:
+                    # Non-blocking wait for message
+                    self.mqtt.check_msg()
+                    # Then need to sleep to avoid 100% CPU usage (in a real
+                    # app other useful actions would be performed instead)
+                    time.sleep(1)
+            except:
+                self.log("Falha no mqtt.wait_msg ou mqtt.check_msg\r\n ")
+                return
 
         self.mqtt.disconnect()
